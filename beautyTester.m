b431a8fb4f9df9335f678f54ceb38704c6200f0d
phelpmad <phelpmad@oregonstate.edu>
2022-09-25 18:10:03 -0700

    Created

---
+++ beautyTester.m
@@ -0,0 +1,12 @@
+function isBeautiful = beautyTester(girlsName)
+%BEAUTYTESTER Summary of this function goes here
+%   Detailed explanation goes here
+
+if girlsName == "Madison"
+    isBeautiful = true;
+else
+    error 'Possibly? More data needed.'
+end
+
+end
+
